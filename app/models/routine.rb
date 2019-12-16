67b9a1b8415ce908161541ea2fcfe374997ff7d6
emily <emily.m.westfall@gmail.com>
2019-12-16 08:27:39 -0500

    add user-routines association in User Model and Routine Model; confirm associations functioning as expected

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -2,4 +2,9 @@ class Routine < ApplicationRecord
     #steps_subdoc will now accept a Hash rather than String
     serialize :steps_subdoc, Hash
     validates :author_id, :title, :steps_subdoc, presence: true
+
+    belongs_to :author,
+        primary_key: :id,
+        foreign_key: :author_id,
+        class_name: :User
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true
     validates :name, :bio, :sunscreen_id, presence: true
+
+    has_many :routines,
+        primary_key: :id,
+        foreign_key: :author_id,
+        class_name: :Routine,
+        dependent: :destroy
 end
