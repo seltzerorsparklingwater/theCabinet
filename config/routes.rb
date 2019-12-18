b5a81f40bd026ea2858327d99d70fd660e9581ee
emily <emily.m.westfall@gmail.com>
2019-12-18 08:35:28 -0500

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:show, :update, :create, :destroy] do
+    resources :routines, only: [:index, :create]
+  end
+
+  resources :routines, only: [:index, :show, :create, :update, :destroy]
+
+
+
 end
